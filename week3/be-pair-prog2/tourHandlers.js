e3d99238929e50242eea2f349819adea24b2d98a
Jan-Krister Ranta <jan.ranta@metropolia.fi>
2024-11-04 18:51:56 +0200

    pp2

--- week3/be-pair-prog2/tourHandlers.js
+++ week3/be-pair-prog2/tourHandlers.js
@@ -1,29 +1,51 @@
 const Tour = require("./tourLib");
 
 const getAllTours = (req, res) => {
-    res.json({ message: "Hello from getAllTours" });
+    const tours = Tour.getAllTours();
+    res.json(tours)
 };
 
 const createTour = (req, res) => {
-    res.json({ message: "Hello from createTour" });
+    const { name, info, image, price } = req.body;
+    const newTour = Tour.createTour(name, info, image, price);
+
+    if (!newTour) return res.status(500)
+    else {
+        res.json(newTour)
+    }
 };
 
 const getTourById = (req, res) => {
-    res.json({ message: "Hello from getTourById" });
+    const tourId = req.params.tourId;
+
+    const response = Tour.getTourById(tourId);
+
+    if (!response) return res.status(500)
+    else return res.json(response)
 };
 
 const updateTour = (req, res) => {
-    res.json({ message: "Hello from updateTour" });
+    const tourId = req.params.tourId
+    const { name, info, image, price } = req.body;
+
+    const response = Tour.updateTour(tourId, { name, info, image, price })
+
+    if (!response) return res.status(500)
+    else return res.json(response)
 };
 
 const deleteTour = (req, res) => {
-    res.json({ message: "Hello from deleteTour" });
+    const tourId = req.params.tourId
+    const response = Tour.deleteTour(tourId);
+
+    if (!response) return res.status(500)
+    else return res.json(response)
 };
 
 module.exports = {
-  getAllTours,
-  getTourById,
-  createTour,
-  updateTour,
-  deleteTour,
+    getAllTours,
+    getTourById,
+    createTour,
+    updateTour,
+    deleteTour,
 };
--- week3/be-pair-prog2/tourLib.js
+++ week3/be-pair-prog2/tourLib.js
@@ -1,7 +1,65 @@
-if (require.main === module) {
-    let result = addOne("7 Days Tour"," Join us for the Best of Helsinki!","https://www.course-api.com/images/tours/tour-x.jpeg", "1,495");
-    console.log(result);
-    console.log("getAll called:", getAll());
-    console.log("findById called:", findById(1));
-    // rest of the tests here
-   }
+const { getTourById, updateTour, deleteTour, createTour } = require("./tourHandlers");
+
+//if (require.main === module) {
+//    let result = addOne("7 Days Tour", " Join us for the Best of Helsinki!", "https://www.course-api.com/images/tours/tour-x.jpeg", "1,495");
+//    console.log(result);
+//    console.log("getAll called:", getAll());
+//    console.log("findById called:", findById(1));
+//    // rest of the tests here
+//}
+
+let toursArray = [];
+let nextId = 0;
+
+function getAllTours() {
+    return toursArray
+}
+
+function getTourById(id) {
+    const tour = toursArray.find((item) => item.id === Number(id));
+
+    if (!tour) return false
+    else return tour;
+}
+
+function createTour(name, info, image, price) {
+    const newTour = {
+        id: nextId++,
+        name,
+        info,
+        image,
+        price
+    }
+
+    toursArray.push(newTour)
+    return newTour;
+}
+
+function updateTour(id, updatedTour) {
+
+    const feedback = getFeedbackById(id);
+
+    if (!feedback) return false;
+    
+    if (tour.sender) tour.sender = updatedTour.sender;
+    if (tour.message) tour.message = updatedTour.message;
+    if (tour.rating) tour.rating = updatedTour.rating;
+
+    return feedback;
+}
+
+function deleteTour(id) {
+    const originalLength = toursArray.length;
+    toursArray = toursArray.filter((item) => item.id !== Number(id));
+    return toursArray.length < originalLength;
+}
+
+Tour = {
+    getAllTours,
+    getTourById,
+    createTour,
+    updateTour,
+    deleteTour
+}
+
+module.exports = Tour;
